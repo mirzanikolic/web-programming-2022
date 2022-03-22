424c643d3d35b4ae7f2052108ca99e6511167853
Mirza Nikolic <mirza.nikolic@gmail.com>
2022-03-22 17:34:27 +0100

    Idemo dalje

--- rest/dao/ProjectDao.class.php
+++ rest/dao/ProjectDao.class.php
@@ -1,14 +1,31 @@
 <?php
-$servername = "sql.freedb.tech";
-$username = "freedb_mirza";
-$password = "?BA2xCExbsPfFrx";
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=freedb_bazatest", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+class ProjectDao{
+
+  private $conn;
+
+  public function __construct(){
+  $servername = "sql.freedb.tech";
+  $username = "freedb_mirza";
+  $password = "?BA2xCExbsPfFrx";
+
+  try {
+    $this->conn = new PDO("mysql:host=$servername;dbname=freedb_bazatest", $username, $password);
+    // set the PDO error mode to exception
+    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+  }
+
+  public function get_all(){
+    $stmt = $$this->conn->prepare("SELECT * FROM Users");
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+
 }
+
 ?>
