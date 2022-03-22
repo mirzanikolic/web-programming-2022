7287d591085ddf3806949e6d13ab00a291b62220
Mirza Nikolic <mirza.nikolic@gmail.com>
2022-03-22 23:28:49 +0100

    update

--- rest/index.php
+++ rest/index.php
@@ -4,8 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once './dao/ProjectDao.class.php';
-require_once './vendor/autoload.php';
+require_once 'dao/ProjectDao.class.php';
+require_once '../vendor/autoload.php';
 
 Flight::register('ProjectDao', 'ProjectDao');
 
