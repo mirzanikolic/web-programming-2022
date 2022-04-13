cff65218fa0f1fadd56a8c434a47248a44c0a830
Mirza Nikolic <mirza.nikolic@gmail.com>
2022-04-13 23:48:38 +0200

    changes

---
+++ rest/dao/ProjectDao.class.php
@@ -0,0 +1,57 @@
+<?php
+
+class ProjectDao{
+
+  private $conn;
+
+  public function __construct(){
+
+  $servername = "sql11.freemysqlhosting.net";
+  $username = "sql11481005";
+  $password = "u1CdridxHm";
+  $schema = "sql11481005";
+
+  try {
+    $this->conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+    // set the PDO error mode to exception
+    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+  }
+
+  public function addUser($users){
+    $stmt = $this->conn->prepare("INSERT INTO Users (name, surname) VALUES (:name, :surname)");
+    $stmt->execute(['name'=>$users['name'], 'surname'=>$users['surname']]);
+    $users['id'] = $this->conn->lastInsertId();
+    return $users;
+  }
+
+  public function getAll(){
+    $stmt = $this->conn->prepare("SELECT * FROM Users");
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function getOne($id){
+    $stmt = $this->conn->prepare('SELECT * FROM Users WHERE id=:id');
+    $stmt->execute(['id' => $id]);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return reset($result);
+  }
+
+  public function updateById($users){
+    $stmt = $this->conn->prepare('UPDATE Users SET name=:name, surname=:surname WHERE id=:id');
+    $stmt->execute($users);
+    return $users;
+  }
+
+  public function delete($id){
+    $stmt = $this->conn->prepare("DELETE FROM Users WHERE id=:id");
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+  }
+}
+
+?>
