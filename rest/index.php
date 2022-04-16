a0d4fb3448209b2305ebb050df6fafc59ec148c3
Mirza Nikolic <mirza.nikolic@gmail.com>
2022-04-16 14:49:26 +0200

    minor changes

--- rest/index.php
+++ rest/index.php
@@ -3,9 +3,9 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 require_once '../vendor/autoload.php';
 require_once 'dao/UserDao.class.php';
-
 require_once 'services/UserService.php';
 
 Flight::register('userService', 'UserService');
