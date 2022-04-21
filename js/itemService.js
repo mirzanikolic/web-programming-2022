040b97f0b9bf8bfac71e1fd5778c4f0c007e4b94
Mirza Nikolic <mirza.nikolic@gmail.com>
2022-04-22 00:59:52 +0200

    cards alligned horizontaly

--- js/itemService.js
+++ js/itemService.js
@@ -1,13 +1,13 @@
 var ItemService = {
-  init: function(){
-      $('#addUserForm').validate({
-        submitHandler: function(form) {
-          var newUser = Object.fromEntries((new FormData(form)).entries());
-          ItemService.add(newUser);
-        }
-      });
-      ItemService.list();
-    },
+  init: function() {
+    $('#addUserForm').validate({
+      submitHandler: function(form) {
+        var newUser = Object.fromEntries((new FormData(form)).entries());
+        ItemService.add(newUser);
+      }
+    });
+    ItemService.list();
+  },
 
   list: function() {
     $.get("rest/users", function(data) {
@@ -17,37 +17,44 @@ var ItemService = {
       var html = "";
       for (let i = 0; i < data.length; i++) {
         html += `
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <div class="card mb-3" style="max-width: 18rem;">
-            <img class="card-img-top" src="img_avatar.png" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">` + data[i].name + `</h5>
-              <p class="card-text">` + data[i].surname + `</p>
-              <div class="btn-group" role="group">
-              <button type="button" class="btn btn-secondary user-button" onclick="ItemService.get(` + data[i].id + `)">Edit</button>
-              <button type="button" class="btn btn-danger users-button" onclick="ItemService.delete(` + data[i].id + `)">Delete</button>
-              </div>
-          </div>
-        </div>
-      </div>`;
+        <div class="row justify-content-center">
+          <div class="card mb-3" style="width: 600px">
+           <div class="row no-gutters">
+               <div class="col-sm-5">
+                   <img class="card-img" src="img_avatar.png" alt="Suresh Dasari Card">
+               </div>
+               <div class="col-sm-7 d-flex flex-colum mb-2">
+                   <div class="card-body">
+                        <h5 class="card-title">` + data[i].name + ` ` + data[i].surname + `</h5>
+                        <p class="card-text">` + data[i].surname + `</p>
+                   </div>
+                   <div class="btn-group align-self-end" role="group">
+                        <button type="button" class="btn btn-secondary user-button" onclick="ItemService.get(` + data[i].id + `)">Edit</button>
+                        <button type="button" class="btn btn-danger users-button" onclick="ItemService.delete(` + data[i].id + `)">Delete</button>
+                    </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+   </div>`;
       }
       $("#users-list").html(html);
     });
   },
 
-  add: function(newUser){
+  add: function(newUser) {
     $.ajax({
       url: 'rest/users',
-        type: 'POST',
-        data: JSON.stringify(newUser),
-        contentType: "application/json",
-        dataType: "json",
-        success: function(result) {
-            $('#addUserModal').modal("hide");
-            $("#users-list").html('<div class="spinner-border" role="status"> <span class="sr-only"></span>  </div>');
-            ItemService.list();
-            toastr.success("New user added succesfully");
-          }
+      type: 'POST',
+      data: JSON.stringify(newUser),
+      contentType: "application/json",
+      dataType: "json",
+      success: function(result) {
+        $('#addUserModal').modal("hide");
+        $("#users-list").html('<div class="spinner-border" role="status"> <span class="sr-only"></span>  </div>');
+        ItemService.list();
+        toastr.success("New user added succesfully");
+      }
     });
   },
 
